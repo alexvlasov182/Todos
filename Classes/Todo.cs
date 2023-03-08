926448056cb436131eb85b0a377c1599e01e4f83
Alex Vlasov <drumlife182@gmail.com>
2023-03-08 18:57:24 +0100

    Added new class to application

---
+++ Classes/Todo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Todos.Classes;
+
+public class Todo
+{
+  public int Id { get; set; }
+  public string? Title { get; set; } = string.Empty;
+  public bool Completed { get; set; } = false;
+}
+
